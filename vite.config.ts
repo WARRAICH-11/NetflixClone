9275bcfc2e72365adc76b0dd52211a9b761d60b2
HASSAN WARRAICH <hm2257153@gmail.com>
2025-10-20 21:27:56 +0500

    min set

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Netflix-Style-Video-Showcase/',
+  base: '/NetflixClone/',
   plugins: [react()],
   resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
